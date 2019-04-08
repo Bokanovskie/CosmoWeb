adefd4e15bf2c740a84f60c6d050c514c60ce704
almat <alexandre.matis@smile.fr>
2019-04-08 16:14:41 +0200

    Init routing: 	- index 	- admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,2 +1,7 @@
 const routes = require('express').Router()
 
+routes.get('/admin', (req, res) => {
+    res.status(200).render('admin_dashboard.html')
+})
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes/routing.js')
+
 const app = express()
 
 app.use(express.static('public'))
@@ -11,9 +12,8 @@ nunjucks.configure(
     express: app
   })
 
-app.get('/', function (req, res) {
-  res.render('index.html')
-})
+app.use('/', routes)
+app.use('/admin', routes)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
